5669bb29c606b798d6f362e60cb042aa65b83813
MeryScarlet15 <meryscarlet15@gmail.com>
2022-09-30 19:53:49 +0200

    feat delete build

--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const graphql_1 = require("./src/lib/graphql/graphql");
-(0, graphql_1.createGraphqlServer)(process.env.PORT || "4000");
-//# sourceMappingURL=index.js.map
