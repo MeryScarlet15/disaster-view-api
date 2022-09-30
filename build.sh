fbf41cfa5be62b91573f335dafc9d4c6727f0ee2
MeryScarlet15 <meryscarlet15@gmail.com>
2022-09-30 19:55:35 +0200

    fix (deploy): add build.sh script

--- build.sh
+++ build.sh
@@ -1,16 +1,8 @@
 rm -rf build
 
-npm run build
+tsc -p .
 
 cp -R src/lib/graphql/typedefs build/src/lib/graphql
-
-git add *
-
-git commit -m "deploy"
-
-git push
-
-git push heroku main
 
 echo "The deploy has finished"
 
