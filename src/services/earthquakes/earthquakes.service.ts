9fbcdc0e8b39f13c314249740c767b647b16575d
MeryScarlet15 <meryscarlet15@gmail.com>
2022-10-01 11:02:51 +0200

    feat add default earthquakes

--- src/services/earthquakes/earthquakes.service.ts
+++ src/services/earthquakes/earthquakes.service.ts
@@ -6,7 +6,7 @@ class EarthquakeService {
     return [{
       id: 1, 
       place_id: 1, 
-      magnitude: "3.8mg",
+      magnitude: "'3.8 M(mb)",
       latitude: '4343214231423142',
       longitude: '-4123412432',
       tsunami: true,
@@ -19,7 +19,33 @@ class EarthquakeService {
     {
       id: 2, 
       place_id: 3, 
-      magnitude: "3.8mg",
+      magnitude: "'3.8 M(mb)",
+      latitude: '4343214231423142',
+      longitude: '-4123412432',
+      tsunami: true,
+      unix_event_time: 142342314342,
+      event_time: 'Sep 30 2022',
+      created_at: 43214,
+      updated_at: 4324231,
+      deleted_at: 143214
+    },
+    {
+      id: 3, 
+      place_id: 3, 
+      magnitude: "'3.8 M(mb)",
+      latitude: '4343214231423142',
+      longitude: '-4123412432',
+      tsunami: true,
+      unix_event_time: 142342314342,
+      event_time: 'Sep 30 2022',
+      created_at: 43214,
+      updated_at: 4324231,
+      deleted_at: 143214
+    },
+    {
+      id: 4, 
+      place_id: 4, 
+      magnitude: "'3.8 M(mb)",
       latitude: '4343214231423142',
       longitude: '-4123412432',
       tsunami: true,
